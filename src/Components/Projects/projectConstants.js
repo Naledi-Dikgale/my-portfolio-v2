9b0eae8e3c1fc8da34e7dd2874b769a22b3591e2
naledi-dikgale <naledi22khosi@gmail.com>
2024-01-02 21:17:40 +0200

    Refactor

---
+++ src/Components/Projects/projectConstants.js
@@ -0,0 +1,45 @@
+// projectConstants.js
+
+export const projectsData = [
+  {
+    title: 'Wine Tasting Invite',
+    description: 'A Wine Tasting Party invitation is a two page website providing details about an extravagant wine tasting event.',
+    stack: ['html5', 'tailwindcss', 'javascript'],
+    livePreviewLink: 'https://naledi-dikgale.github.io/capstone1-project/',
+    viewCodeLink: 'https://github.com/Naledi-Dikgale/capstone1-project.git',
+    imageSrc: 'wine.PNG',
+  },
+  
+  // {
+  //   title: 'To Do List App',
+  //   description: 'This Todo-list app was created using JavaScript, Items can be added, edited, stored and deleted from the list.',
+  //   stack: ['html5', 'css3', 'javascript'],
+  //   livePreviewLink: 'https://naledi-dikgale.github.io/todo-list/dist/',
+  //   viewCodeLink: 'https://github.com/Naledi-Dikgale/react-todo.git',
+  //   imageSrc: 'todolist.png',
+  // },
+  {
+    title: 'Math Magicians',
+    description: 'A website for all fans of mathematics, it allows users to make simple calculations and displays random motivational quotes.',
+    stack: ['html5', 'css3', 'javascript'],
+    livePreviewLink: 'https://naledis-calculator.onrender.com/',
+    viewCodeLink: 'https://github.com/Naledi-Dikgale/math-magician-project',
+    imageSrc: 'math.PNG',
+  },
+  {
+    title: 'Console Hub',
+    description: 'A Full Stack app for Gaming consoles rentals. It provides users with amazing experience and beautiful UI.',
+    stack: ['react', 'rubyonrails', 'tailwindcss'],
+    livePreviewLink: 'https://consolehub.netlify.app/',
+    viewCodeLink: 'https://github.com/stephenkati/gaming_console_reservation_front_end',
+    imageSrc: 'console.PNG',
+  },
+  {
+    title: 'Stock Market App',
+    description: 'This is an application that displays a list of businesses and allows the user to filter and search for details of the business.',
+    stack: ['javascript', 'react', 'html5', 'css3'],
+    livePreviewLink: 'https://stock-market-2xyl.onrender.com',
+    viewCodeLink: 'https://github.com/Naledi-Dikgale/react-capstone.git',
+    imageSrc: 'stock.PNG',
+  },
+];
