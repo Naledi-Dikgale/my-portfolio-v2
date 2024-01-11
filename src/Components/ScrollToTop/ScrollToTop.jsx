2c0d216e7cde58342b6e25cc2e1bbb32507b6643
naledi-dikgale <naledi22khosi@gmail.com>
2024-01-11 13:35:52 +0200

    Fix linter errors

--- src/Components/ScrollToTop/ScrollToTop.jsx
+++ src/Components/ScrollToTop/ScrollToTop.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import "./ScrollToTop.css";
 
