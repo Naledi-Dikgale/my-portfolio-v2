d0e23ad23f3edd658672dd5f5870574eedb1fe36
naledi-dikgale <naledi22khosi@gmail.com>
2024-04-10 23:16:54 +0200

    Update changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,26 +1,3 @@
-// import React from "react";
-// import "./Footer.css";
-// import { ThemeContext } from "../../Context/theme";
-
-// export const Footer = () => {
-//   const [{ themename }] = React.useContext(ThemeContext);
-
-//   return (
-//     <>
-//       <div className={"section " + themename}>
-//         <a href="/" rel="noreferrer" target="_blank">
-//           <div className="footerBox">
-//             <h5 className="footer">
-//            Copyright ©2024 Naledi Dikgale{" "}
-//            - All rights Reserved
-//             </h5>
-//           </div>
-//         </a>
-//       </div>
-//     </>
-//   );
-// };
-
 import React from "react";
 import "./Footer.css";
 import { ThemeContext } from "../../Context/theme";
