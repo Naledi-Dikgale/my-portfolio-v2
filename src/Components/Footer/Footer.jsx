69fd4ea6f917dd7e451707d98dce8dd517a4c277
naledi-dikgale <naledi22khosi@gmail.com>
2024-05-31 10:00:50 +0200

    Update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import "./Footer.css";
 import { ThemeContext } from "../../Context/theme";
 import Image from "./../../assets/logo.png";
-import { FaLinkedin } from "react-icons/fa";
-import { VscGithub } from "react-icons/vsc";
-import { CgMail } from "react-icons/cg";
 
 export const Footer = () => {
   const [{ themename }] = React.useContext(ThemeContext);
@@ -15,37 +12,6 @@ export const Footer = () => {
         <div className="logo-section">
           <img src={Image} alt="N" className="logo" />
           <h5 className="company-name">Naledi Dikgale</h5>
-        </div>
-      </div>
-      <div className="footer-section">
-        <div className="social-links">
-          <a
-            href="https://www.linkedin.com/in/naledi-dikgale/"
-            target="_blank"
-            rel="noreferrer"
-            className="social-link"
-          >
-            <FaLinkedin className="linkedin" />
-            {/* <span className="social-media-name">LinkedIn</span> */}
-          </a>
-          <a
-            href="https://github.com/Naledi-Dikgale"
-            target="_blank"
-            rel="noreferrer"
-            className="social-link"
-          >
-            <VscGithub className="github" />
-            {/* <span className="social-media-name">GitHub</span> */}
-          </a>
-          <a
-            href="mailto:nalediaccc@gmail.com"
-            target="_blank"
-            rel="noreferrer"
-            className="social-link"
-          >
-            <CgMail className="email" />
-            {/* <span className="social-media-name">Email</span> */}
-          </a>
         </div>
       </div>
       <div className="footer-section">
